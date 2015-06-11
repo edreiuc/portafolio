67ab8d9d0cfd9f6eac09e798825ea1af2d959179
edreiuc <sam_edreiuc@live.com>
2015-06-11 10:22:55 -0500

    font-awesome and contact page

--- contact.php
+++ contact.php
@@ -4,11 +4,31 @@
 	include 'header.php';
 ?>
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<h1>Contactame</h1>
-			
+		<div class="col-md-6 col-md-offset-3">
+			<div class="page-header">
+			  <h1>Contactame <small><i class="glyphicon glyphicon-user"></i></small></h1>
+			</div>
+			<div class="panel panel-default">
+			  <div class="panel-body">
+			    <ul>
+			    	<li><i class="fa fa-envelope-square"></i>  sam_edreiuc@live.com</li>
+			    	<li><i class="fa fa-envelope-square"></i>  ucangulosamuel@gmail.com</li>
+			    	<li><i class="fa fa-linkedin-square"></i>  Linkedin</li>
+			    	<li><i class="fa fa-twitter-square"></i>  @Uc_Samuel</li>
+			    </ul>
+			  </div>
+			</div>
 		</div>
 	</div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
 <?php
 	include 'footer.php';
 ?>
